066cc1914d927de8a832181e82852deb10197bd6
Soly <82676469+Solyphonous@users.noreply.github.com>
2024-10-20 15:54:40 +0100

    Implement jambonian api

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,43 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+interface Env {
+	JAMBOS_KV: KVNamespace
+}
+
+function formatError(error: string) {
+	const obj = {"error": error}
+	return JSON.stringify(obj)
+}
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		const url = new URL (request.url)
+		const path = url.pathname.replace(/^\/api/, "")
+
+		const articlepath = /^\/article\/(.*)$/.exec(path)
+
+		if (articlepath) {
+			const articleName = articlepath[1].replace(/-/g, " ")
+			const article = await env.JAMBOS_KV.get(articleName)
+
+			if (article) {
+				return new Response(article, { 
+					status: 200,
+					headers: {
+						"Content-Type": "application/json" 
+					}
+				})
+			}
+			else {
+				return new Response(formatError("Article not found!"), { status: 404 })
+			}
+		}
+
+		if (path === "/list") {
+			const list = await env.JAMBOS_KV.list()
+			if (list === null) {
+				return new Response("Failed kv fetch", { status: 404 })
+			}
+			return new Response(JSON.stringify(list.keys), { status: 200})
+		}
+		return new Response("Invalid api request", { status: 404 })
 	},
 } satisfies ExportedHandler<Env>;
